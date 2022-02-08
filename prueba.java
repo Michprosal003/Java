7d0cf83afd8cd06612d8878625a2e572da0a9f9a
Michprosal003 <95501615+Michprosal003@users.noreply.github.com>
2022-02-08 13:46:02 -0500

    Add files via upload

---
+++ prueba.java
@@ -0,0 +1,25 @@
+
+
+import java.util.Scanner;
+
+public class prueba {
+
+    static Scanner Entrada= new Scanner(System.in); 
+
+     public static void main (String[] args){
+
+      
+        int i=100;
+        while(i>0){
+
+
+      System.out.println(""+i);
+
+      i-=1;
+
+
+
+        }
+
+     }
+}
